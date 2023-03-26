3c884b47f24e198591e54a6bdae0c14ee93b4cd3
Horacio <horacio.junior@ibm.com>
2023-03-26 12:19:39 -0300

    Tratamento de status http

--- cars/src/main/java/com/example/cars/api/CarrosController.java
+++ cars/src/main/java/com/example/cars/api/CarrosController.java
@@ -3,6 +3,8 @@ package com.example.cars.api;
 import com.example.cars.domain.Carro;
 import com.example.cars.domain.CarroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,34 +26,51 @@ public class CarrosController {
     CarroService service;
 
     @GetMapping
-    Iterable<Carro> getCarros() {
-        return service.getCarros();
+    ResponseEntity<Iterable<Carro>> getCarros() {
+        return ResponseEntity.ok(service.getCarros());
+//        return new ResponseEntity<>(service.getCarros(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public Optional<Carro> getById(@PathVariable("id") Long id){
-        return  service.getById(id);
+    public ResponseEntity<Carro> getById(@PathVariable("id") Long id) {
+
+        Optional<Carro> carro = service.getById(id);
+
+        return carro.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
+        // if ternário
+//      return   service.getById(id).isPresent() ?
+//                ResponseEntity.ok(service.getById(id).get()) :
+//                ResponseEntity.notFound().build();
+        // if comum
+//        if (service.getById(id).isPresent()) {
+//            return ResponseEntity.ok(service.getById(id).get());
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
     }
 
     @GetMapping("/tipo/{tipo}")
-    public Iterable<Carro> getByTipo(@PathVariable("tipo") String tipo){
-        return service.getByTipo(tipo);
+    public ResponseEntity getByTipo(@PathVariable("tipo") String tipo) {
+        return service.getByTipo(tipo).isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(service.getByTipo(tipo));
     }
 
     @PostMapping
-    public String saveCarro(@RequestBody Carro carro){
+    public String saveCarro(@RequestBody Carro carro) {
         Carro carroSalvo = service.saveCarro(carro);
-        return "Carro "+carroSalvo.getNome()+" Salvo!";
+        return "Carro " + carroSalvo.getNome() + " Salvo!";
     }
 
     @PutMapping("/id/{id}")
-    public String updateCarro(@RequestBody Carro carro, @PathVariable("id") Long id){
-        Carro updated =  service.updateCarro(carro, id);
-        return "Carro "+updated.getNome()+" Updated!";
+    public String updateCarro(@RequestBody Carro carro, @PathVariable("id") Long id) {
+        Carro updated = service.updateCarro(carro, id);
+        return "Carro " + updated.getNome() + " Updated!";
     }
+
     @DeleteMapping("/id/{id}")
-    public String deleteCarro(@PathVariable("id") Long id){
+    public String deleteCarro(@PathVariable("id") Long id) {
         Carro deleted = service.deleteById(id);
-        return "Carro "+deleted.getNome()+ " Deletado!";
+        return "Carro " + deleted.getNome() + " Deletado!";
     }
 }
